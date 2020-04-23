6f05efa70be83fffa36a2a684f63744af768449f
ayauri29 <albayauri.29@gmail.com>
2020-04-23 14:18:30 -0500

    CRUD of users

---
+++ src/components/About.js
@@ -0,0 +1,25 @@
+import React, { Fragment } from "react";
+
+export const About = () => (
+	<Fragment>
+		<h1>about</h1>
+		<p>
+			Lorem ipsum dolor sit amet consectetur adipiscing elit, velit turpis
+			senectus diam duis faucibus natoque taciti, ante ridiculus potenti iaculis
+			dui venenatis. A vivamus tortor sociis lectus massa proin sapien mauris
+			pretium vitae, class commodo eget cursus penatibus nam auctor ac felis.
+			Vestibulum phasellus fringilla potenti neque netus eleifend aliquet mollis
+			sapien, elementum commodo cras augue suspendisse aptent euismod molestie,
+			condimentum etiam gravida platea dictum nulla porttitor arcu.
+		</p>
+		<p>
+			Lorem ipsum dolor sit amet consectetur adipiscing elit, velit turpis
+			senectus diam duis faucibus natoque taciti, ante ridiculus potenti iaculis
+			dui venenatis. A vivamus tortor sociis lectus massa proin sapien mauris
+			pretium vitae, class commodo eget cursus penatibus nam auctor ac felis.
+			Vestibulum phasellus fringilla potenti neque netus eleifend aliquet mollis
+			sapien, elementum commodo cras augue suspendisse aptent euismod molestie,
+			condimentum etiam gravida platea dictum nulla porttitor arcu.
+		</p>
+	</Fragment>
+);
